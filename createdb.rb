c35cf4fff6c35159d4f96b07149250ec0530661d
sroman-537 <sam.roman@kellogg.northwestern.edu>
2020-03-12 02:29:07 +0000

    Final Update #2

--- createdb.rb
+++ createdb.rb
@@ -1,10 +1,12 @@
 # Set up for the application and database. DO NOT CHANGE. #############################
 require "sequel"                                                                      #
 connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
-DB = Sequel.connect(connection_string)                                                #
+DB ||= Sequel.connect(connection_string)                                              #
 #######################################################################################
 
 # Database schema - this should reflect your domain model
+
+# New domain model - adds users
 DB.create_table! :events do
   primary_key :id
   String :title
@@ -15,21 +17,28 @@ end
 DB.create_table! :rsvps do
   primary_key :id
   foreign_key :event_id
+  foreign_key :user_id
   Boolean :going
+  String :comments, text: true
+end
+DB.create_table! :users do
+  primary_key :id
   String :name
   String :email
-  String :comments, text: true
+  String :password
 end
 
 # Insert initial (seed) data
 events_table = DB.from(:events)
 
-events_table.insert(title: "Bacon Burger Taco Fest", 
-                    description: "Here we go again bacon burger taco fans, another Bacon Burger Taco Fest is here!",
-                    date: "June 21",
-                    location: "Kellogg Global Hub")
+events_table.insert(title: "Paris", 
+                    description: "The most romantic trip we've ever experienced!",
+                    date: "2013",
+                    location: "Paris, France")
 
-events_table.insert(title: "Kaleapolooza", 
-                    description: "If you're into nutrition and vitamins and stuff, this is the event for you.",
-                    date: "July 4",
-                    location: "Nowhere")
+events_table.insert(title: "Bangkok", 
+                    description: "The most eventful trip we've ever experienced!",
+                    date: "2016",
+                    location: "Bangkok, Thailand")
+
+puts "Success!"
